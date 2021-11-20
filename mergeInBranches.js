8001635a30b1f8a3f7963eb54adec5889a19135e
igolus <igolus@gmail.com>
2021-11-20 15:32:56 +0100

    dev

--- mergeInBranches.js
+++ mergeInBranches.js
@@ -5,20 +5,29 @@ const mainBranch = "";
 
 const run = async () => {
 
-    for (let i = 0; i < branches.length; i++) {
-        const branch = branches[i];
-        await git.checkout(branch);
-        const mergeSummary = await git.mergeFromTo(mainBranch, branch, {
-            '--no-ff': true,
-        })
-        console.log(mergeSummary);
-        const commitSummary = await git.commit("auto merge")
-        console.log(commitSummary);
+    try {
+        for (let i = 0; i < branches.length; i++) {
+            const branch = branches[i];
+            const checkoutResult =  await git.checkout(branch);
+            const mergeSummary = await git.mergeFromTo(mainBranch, branch, {
+                '--no-ff': true,
+            })
+            console.log(mergeSummary);
+            const commitSummary = await git.commit("auto merge")
+            console.log(commitSummary);
 
-        const pushSummary = await git.push()
-        console.log(pushSummary);
+            const pushSummary = await git.push()
+            console.log(pushSummary);
+        }
     }
-    await git.checkout(mainBranch);
+    catch (err) {
+        console.log(err)
+    }
+    finally {
+        await git.checkout(mainBranch);
+    }
+
+
 }
 
 run()
