d4597b326cc085d2ef6e44361afa9095b6b6987a
igolus <igolus@gmail.com>
2021-11-20 14:10:15 +0100

    dev

--- mergeInBranches.js
+++ mergeInBranches.js
@@ -1,21 +1,28 @@
 const git = require('simple-git')()
 
-git.checkout("brunchClub")
-.then(() => {
-git.mergeFromTo("master", "brunchClub", {
-    '--no-ff': true,
-})
-    .then(mergeSummary => {
-        console.log(mergeSummary)
-        git.commit("aut merge")
-            .then(commitSummary => {
-                console.log(commitSummary)
-                git.push()
-                    .then(pushtSummary => {
-                        console.log(pushtSummary)
-                    })
+const branches = require('./branches.json');
+
+branches.forEach(branchName =>
+    git.checkout(branchName)
+        .then(() => {
+            git.mergeFromTo("master", branchName, {
+                '--no-ff': true,
             })
-    })
-})
+                .then(mergeSummary => {
+                    console.log(mergeSummary)
+                    git.commit("aut merge")
+                        .then(commitSummary => {
+                            console.log(commitSummary)
+                            git.push()
+                                .then(pushtSummary => {
+                                    console.log(pushtSummary)
+                                })
+                        })
+                })
+        })
+
+)
+
+
 
 
