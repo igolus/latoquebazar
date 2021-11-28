16b6c00e465079826856361afd73027734001840
igolus <igolus@gmail.com>
2021-11-28 12:19:17 +0100

    dev

--- pages/specialPage/[id].tsx
+++ pages/specialPage/[id].tsx
@@ -95,6 +95,7 @@ export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
     return await getStaticPropsUtil();
+    
 }
 
 export default ProductDetails
