7156b81d9d172b52ba9f551a6fe2be98836f1bf2
igolus <igolus@gmail.com>
2022-01-13 19:42:20 +0100

    build condition

---
+++ buildCondition.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+buildBranch="hubrise"
+#echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+branch=`git rev-parse --abbrev-ref HEAD`
+#echo $branch
+#echo $buildBranch
+
+if [[ $branch == $buildBranch ]]; then
+  echo "✅ - Build can proceed"
+  exit 1;
+else
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
