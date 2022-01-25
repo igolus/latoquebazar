e0a3b945cba6889bb560d9efc01c7fec995e8c50
igolus <igolus@gmail.com>
2022-01-25 13:16:08 +0100

    dev

--- src/util/displayUtil.js
+++ src/util/displayUtil.js
@@ -560,8 +560,8 @@ export function filterCat(categories, products, deals) {
     })
 
 
-    //return (categories || []).filter(cat => allCatProductIds.includes(cat.id));
-    return categories;
+    return (categories || []).filter(cat => allCatProductIds.includes(cat.id));
+    //return categories;
 }
 
 
