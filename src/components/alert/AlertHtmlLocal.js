549a7df2626770359b798b92fc3e3b248568a793
igolus <igolus@gmail.com>
2022-12-01 09:46:47 +0100

    update conf

--- src/components/alert/AlertHtmlLocal.js
+++ src/components/alert/AlertHtmlLocal.js
@@ -5,24 +5,14 @@ import Typography from "@material-ui/core/Typography";
 function AlertHtmlLocal({severity, title, content, children, marginBottom}) {
     return (
         <>
-        <Alert severity={severity} style={{marginBottom: marginBottom || 2}} variant="outlined" >
-            {title &&
-                <AlertTitle style={{fontWeight: 600, fontSize: "16px"}} textAlign="left">{title}</AlertTitle>
-            }
-
-            {/*{content}*/}
-            {content &&
-
-            <Typography>
-                {content}
-            </Typography>
-            // <div style={{textAlign: "left", overflow: "auto"}}>
-            //     {Parser(content)}
-            // </div>
-            }
-            {children}
-        </Alert>
-
+            <Alert severity={severity} style={{marginBottom: marginBottom || 2}} variant="outlined" title={title}>
+                {content &&
+                    <Typography>
+                        {content}
+                    </Typography>
+                }
+                {children}
+            </Alert>
         </>
     )
 }
